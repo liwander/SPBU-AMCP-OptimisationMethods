d49e1ac75c9855e18c82be5918a910397a2c1075
liwander <gadzhiev.anver@gmail.com>
2023-09-19 18:23:11 +0300

    loss function constructor was implemented

--- 2task/main.py
+++ 2task/main.py
@@ -1,9 +1,22 @@
 import json
+from  functools import reduce
 
-personal_data = {}
+personalData = {}
 with open('./config.json', 'r') as f:
-    personal_data = json.load(f)
+    personalData = json.load(f)
 
-coefs = [   len(personal_data['name']),
-            len(personal_data['surname']),
-            len(personal_data['patronymic'])]
+coefs = [   len(personalData['name']),
+            len(personalData['surname']),
+            len(personalData['patronymic'])]
+
+def constructLossFunction(coefs):
+    def lossFunction(vecX):
+        print(coefs)
+        if len(vecX) != 4:
+            raise TypeError('argument vector must contain 4 items')
+        quadraticPart = reduce(lambda x, y : x + y, [coefs[i] * (vecX[i] ** 2) for i in range(len(vecX) -1)]) \
+                        + (coefs[0] + coefs[2]) * (vecX[3] ** 2)
+        linearPart = reduce(lambda x, y : x + y, [coefs[i] * vecX[i] for i in range(len(vecX) - 1)]) \
+                        + (coefs[0] + coefs[2])
+        return quadraticPart + linearPart
+    return lossFunction

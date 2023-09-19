5f8d689f1aa9898dcd9e98255b8ca5242b53d4fa
liwander <gadzhiev.anver@gmail.com>
2023-09-19 18:57:21 +0300

    function calls counter was implemented and scalar function was moved from task1

--- 2task/main.py
+++ 2task/main.py
@@ -1,5 +1,15 @@
-import json
 from  functools import reduce
+import math as m
+import json
+
+def counted(function):
+
+    def func_wrapper(*args, **kvargs):
+        func_wrapper.callsNumber += 1
+        return function(*args, **kvargs)
+    
+    func_wrapper.callsNumber = 0
+    return func_wrapper
 
 personalData = {}
 with open('./config.json', 'r') as f:
@@ -9,9 +19,9 @@ coefs = [   len(personalData['name']),
             len(personalData['surname']),
             len(personalData['patronymic'])]
 
-def constructLossFunction(coefs):
+def constructLossVecFunction(coefs):
+    @counted
     def lossFunction(vecX):
-        print(coefs)
         if len(vecX) != 4:
             raise TypeError('argument vector must contain 4 items')
         quadraticPart = reduce(lambda x, y : x + y, [coefs[i] * (vecX[i] ** 2) for i in range(len(vecX) -1)]) \
@@ -19,4 +29,12 @@ def constructLossFunction(coefs):
         linearPart = reduce(lambda x, y : x + y, [coefs[i] * vecX[i] for i in range(len(vecX) - 1)]) \
                         + (coefs[0] + coefs[2])
         return quadraticPart + linearPart
+    return lossFunction
+
+def constructLossVecFunction(coefs):
+    
+    @counted
+    def lossFunction(x):
+        return reduce(lambda x, y : x + y, [m.e ** ((coefs[i] * 1e-1) * x) for i in range(len(coefs))]) \
+                                            - (sum(coefs)) * m.sin(x)
     return lossFunction

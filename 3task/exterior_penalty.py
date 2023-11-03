16c81ae17a3d2ae433f9134f477b474227a9272e
liwander <gadzhiev.anver@gmail.com>
2023-11-03 16:50:50 +0300

    coeffs was fixed

--- 3task/exterior_penalty.py
+++ 3task/exterior_penalty.py
@@ -1,7 +1,6 @@
 from common import *
 from unconditional_optim import gradDescVariableStep as minimize
 from collections.abc import Callable
-
 # @call_counted
 
 
@@ -21,13 +20,15 @@ def extPenaltyCoef(step: float) -> float:
 
 def generateExtPenaltiedFunc(step: float) -> Callable[[vector], float]:
     r = extPenaltyCoef(step)
-    phi = lambda x : objectFunction(x) + r * extPenaltyFunction(x)
+    phi = lambda x : np.cosh(cfs[0] * x[0]) + np.cosh(cfs[2] * x[1]) + x[0] + cfs[1] * x[1] + \
+        r * (max(0, x[0] - cf * x[1]) ** 2)
     return phi
 
 
 def generateExtPenaltiedFuncGrad(step: float) -> Callable[[vector], vector]:
     r = extPenaltyCoef(step)
-    phistreak = lambda x : objectFunctionGradient(x) + r * extPenaltyFunctionGradient(x)
+    phistreak = lambda x : np.array([cfs[0] * np.sinh(cfs[0] * x[0]) + 1,
+                          cfs[2] * np.sinh(cfs[2] * x[1]) + cfs[1]]) + r * max(0, x[0] - cf * x[1]) * 2 * np.array([1, -cf])
     return phistreak
 
 

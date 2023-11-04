d1e10a50c207675fabf903d105feb52f342c3f26
liwander <gadzhiev.anver@gmail.com>
2023-11-04 16:24:31 +0300

    uncoditional optimizational method for interior supermethod

--- 3task/uncmin.py
+++ 3task/uncmin.py
@@ -1,46 +1,53 @@
 from collections.abc import Callable
 import numpy as np
-from common import constraintFunction
 
 vector = np.array
 
-
-def stepl(f : Callable[[float], float] ) -> float:
-    a, b, eps = 0, 1, 1e-2
-    n = int(np.ceil((b - a) / eps))  # количество разбиений интервала
-    delta = (b - a) / n  # шаг разбиения
-    x_min = a + delta  # начальное значение аргумента минимума
-    f_min = f(x_min)  # начальное значение функции в точке минимума
-
-    for i in range(1, n + 1):
-        x = a + i * delta  # текущее значение аргумента
-        
-        if constraintFunction()
-            f_x = f(x)  # текущее значение функции
-            if f_x < f_min:
-                x_min = x
-                f_min = f_x
-
-    return x_min
-
-
-def uncmin(
-        f: Callable[[vector], float],
-        grad: Callable[[vector], vector],
-        initx: vector,
+def gradDescVariableStep(
+        func: Callable[[vector], float],
+        funcGrad: Callable[[vector], vector],
+        fconstr: Callable[[vector], float],
+        stepLength: float = 1,
+        initx: vector = np.array([0,0]),
         eps: float = 1e-3,
-        file = None
+        # file = None
 ) -> vector:
-
-    x = initx
-    grx = grad(x)
+    
+    xk = initx
+    fxk = func(xk)
+    gradxk = funcGrad(xk)
     iter = 0
+    # coef = lambda iter : 1 / (iter + 1)
+    coef = 5 / 8
 
-    while (np.linalg.norm(grx) > eps):
+    # file.write("Метод градиентного спуска с переменынм шагом\n")
+    # file.write(
+    #     "It\tX\t\t\t\t\t\t\t\t\t\t\t\tF(x)\t\tAlpha\t\tgrad F\t\t\t\t\t\t\t\t\tТруд. F\t Труд. grad\n")
+   
+
+    while (np.linalg.norm(gradxk) > eps) :
         iter += 1
-        grx = grad(x)
-        minarg = lambda a : f(x - a * grx)
-        x -= grx * stepl(minarg)
+        x = xk - gradxk * stepLength
+        fx = func(x)
 
-    print(iter, f(x))
-    return x
+        # print(iter, x, np.linalg.norm(gradxk))
+
+        if fconstr(x) >= 0 or fx - fxk > -stepLength * 1e-3 * (np.linalg.norm(gradxk) ** 2):
+            stepLength *= coef
+            continue
+
+        # file.write(f"{iter}\t{hexVec(x)}\t{fx}\t{stepLength : 8.4f} \
+        #            \t{(hexVec(gradxk))}\t{func.callsNumber}\t{funcGrad.callsNumber}\n")
+            
+        
+        xk = x
+        fxk = fx
+        gradxk = funcGrad(xk)
+
+    # f.write("X_min = ", res)
+    # print("min = ", fun(res))
+    # print("Трудоемкость по f: ", fun.callsNumber)
+    # print("Трудоемкость по grad f: ", grad.callsNumber)
+
+    print(iter)
+    return xk

0336a0494f8a22178f2fe0617750a4f5a903fac0
liwander <gadzhiev.anver@gmail.com>
2023-11-04 16:30:48 +0300

    method suited for its own uncond optimization

--- 3task/interior_penalty.py
+++ 3task/interior_penalty.py
@@ -1,5 +1,5 @@
 from common import *
-from unconditional_optim import gradDescVariableStep as minimize
+from uncmin import gradDescVariableStep as minimize
 from collections.abc import Callable
 
 
@@ -16,36 +16,34 @@ def barrierFunctionGradient(x: vector) -> float:
 
 
 def barrierCoef(step: float) -> float:
-    return 10.0 ** step
+    return 10 ** (-step)
+    # return 1e-1
 
 
 def generateBarrieredFunc(step: float) -> Callable[[vector], float]:
-    r = barrierCoef(step)
-    def phi(x): return objectFunction(x) + r * barrierFunction(x)
+    def phi(x): return objectFunction(x) + barrierCoef(step) * barrierFunction(x)
+    # def phi(x) : return - r / (x[0] - x[1] * cf) + np.cosh(cfs[0] * x[0]) + np.cosh(cfs[2] * x[1]) + x[0] + cfs[1] * x[1]
     return phi
 
 
 def generateBarrieredFuncGrad(step: float) -> Callable[[vector], vector]:
-    r = barrierCoef(step)
-    def phistreak(x): return objectFunctionGradient(x) + r * barrierFunctionGradient(x)
+    def phistreak(x): return objectFunctionGradient(x) + barrierCoef(step) * barrierFunctionGradient(x)
+    # def phistreak(x) : return np.array([cfs[0] * np.sinh(cfs[0] * x[0]) + 1,
+    #         cfs[2] * np.sinh(cfs[2] * x[1]) + cfs[1]]) + r * (1 / (x[0] - x[1] * cf) ** 2) * np.array([1, -cf])
     return phistreak
 
 
-
-
-
 def intpen():
-    print(barrierCoef(1)*barrierFunction(np.array([ -2.873583,-50.75413516])))
+    # print(barrierCoef(1)*barrierFunction(np.array([ -2.873583,-50.75413516])))
     x = np.array([cf - 2, 1])
-    xprev = x * 1000
+    # xprev = x * 1000
     k = 0
-    # while abs(barrierCoef(k)*barrierFunction(x)) > 1e-2:
-    while np.linalg.norm(x - xprev) > 1e-2:
-        print(k + 1, x)
+    while abs(barrierCoef(k)*barrierFunction(x)) > 1e-2:
+        print(k + 1, x, np.sign(constraintFunction(x)))
         f = generateBarrieredFunc(k)
         fstreak = generateBarrieredFuncGrad(k)
-        xprev = x
-        x = minimize(f, fstreak, initx=x)
+        # xprev = x
+        x = minimize(f, fstreak, constraintFunction, initx=x)
         k += 1
         # print('\n==================\n')
 
